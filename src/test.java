cc68822290def7e28568e062c7cddca68ad2a693
learnszl <ahsongzhili@163.com>
2021-02-04 21:17:16 +0800

    初次提交

---
+++ src/test.java
@@ -0,0 +1,11 @@
+/**
+ * @Author szl
+ * @Date 2021/2/4 21:15
+ * @Description
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+    }
+}

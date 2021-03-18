7c9487bc8c78a6d162448b318c3b96e9bf67fb6f
learnszl <ahsongzhili@163.com>
2021-03-18 16:45:33 +0800

    File类的使用

--- io/src/FileTest.java
+++ io/src/FileTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @Author szl
@@ -9,14 +10,13 @@ import java.io.File;
  */
 public class FileTest {
     @Test
-    public void test1(){
+    public void test1() throws IOException {
         //构造器1
         File file1 = new File("hello.txt");//仅仅是内存层面的对象，不要求硬盘中有实际的文件
         System.out.println(file1);
-
-
+        if (!file1.exists()) {
+            boolean newFile = file1.createNewFile();
+            System.out.println(newFile);
+        }
     }
-
-
-
 }
